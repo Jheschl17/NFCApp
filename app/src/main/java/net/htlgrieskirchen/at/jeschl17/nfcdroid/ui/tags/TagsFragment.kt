bcd97f89322c7cf0e10380f278bf7936299282bb
jonas hescl <heschl@protonmail.com>
2020-05-27 12:25:57 +0200

    add layouts for `tag` tab

--- app/src/main/java/net/htlgrieskirchen/at/jeschl17/nfcdroid/ui/tags/TagsFragment.kt
+++ app/src/main/java/net/htlgrieskirchen/at/jeschl17/nfcdroid/ui/tags/TagsFragment.kt
@@ -1,4 +1,4 @@
-package net.htlgrieskirchen.at.jeschl17.nfcdroid.ui.profiles
+package net.htlgrieskirchen.at.jeschl17.nfcdroid.ui.tags
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,14 +10,14 @@ import net.htlgrieskirchen.at.jeschl17.nfcdroid.R
 /**
  * A simple [Fragment] subclass.
  */
-class ProfilesFragment : Fragment() {
+class TagsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profiles, container, false)
+        return inflater.inflate(R.layout.fragment_tags, container, false)
     }
 
 }
