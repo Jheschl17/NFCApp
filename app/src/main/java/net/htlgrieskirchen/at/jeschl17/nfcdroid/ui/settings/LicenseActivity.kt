139cd6ebffbe225e8e888cf4b2f130ec9f5843ba
Jonas <heschl@protonmail.com>
2020-07-01 04:00:40 +0200

    implement preferences

---
+++ app/src/main/java/net/htlgrieskirchen/at/jeschl17/nfcdroid/ui/settings/LicenseActivity.kt
@@ -0,0 +1,12 @@
+package net.htlgrieskirchen.at.jeschl17.nfcdroid.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.htlgrieskirchen.at.jeschl17.nfcdroid.R
+
+class LicenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+    }
+}
--- app/src/main/java/net/htlgrieskirchen/at/jeschl17/nfcdroid/ui/settings/SettingsFragment.kt
+++ app/src/main/java/net/htlgrieskirchen/at/jeschl17/nfcdroid/ui/settings/SettingsFragment.kt
@@ -1,24 +1,38 @@
 package net.htlgrieskirchen.at.jeschl17.nfcdroid.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
 import net.htlgrieskirchen.at.jeschl17.nfcdroid.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        findPreference<Preference>(getString(R.string.licenses))?.setOnPreferenceClickListener {
+            return@setOnPreferenceClickListener true
+        }
+        findPreference<Preference>(getString(R.string.source_code))?.setOnPreferenceClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://gitlab.com/_cicero/NFCApp"))
+            startActivity(intent)
+            return@setOnPreferenceClickListener true
+        }
+        findPreference<Preference>(getString(R.string.icons8))?.setOnPreferenceClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://icons8.com/"))
+            startActivity(intent)
+            return@setOnPreferenceClickListener true
+        }
     }
 
 }
